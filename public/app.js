75741c97b77b343b21e9ea7dd484521ad80c494f
Neo Temores <neo@Mac.lan>
2022-05-23 17:40:20 -0500

    working on login feature

--- public/app.js
+++ public/app.js
@@ -1,5 +1,63 @@
 
 
+let loginButton = document.querySelector('#loginButton')
+loginButton.addEventListener('click', login)
+
+let passwordBox = document.querySelector('#password')
+passwordBox.addEventListener('keypress', (e) => {
+    if (e.key == 'Enter') { return login() }
+})
+
+function login() {
+    let inputUserName = document.querySelector('#username')
+    let inputPassword = document.querySelector('#password')
+
+    fetch('http://localhost:8000/api/users')
+        .then(response => response.json())
+        .then(data => authenticate(data, inputUserName, inputPassword))
+}
+
+function authenticate(data, inputUserName, inputPassword) {
+    let username = inputUserName.value
+    let password = inputPassword.value
+
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].user_name === username && data[i].password === password) {
+            console.log('logged in');
+            return showHomePage();
+        }
+    }
+    return showSignUpMsg()
+}
+
+function showHomePage() {
+    let homePage = document.querySelector('#homePage')
+    homePage.classList.remove('hide')
+
+    let loginPage = document.querySelector('#loginPage')
+    loginPage.classList.add('hide')
+}
+
+function showSignUpMsg() {
+    let signUpMessage = document.querySelector('#signUpMessage')
+    signUpMessage.classList.remove('hide');
+}
+
+let signUpLink = document.querySelector('#signUpLink')
+signUpLink.addEventListener('click', showSignUpPage);
+
+function showSignUpPage() {
+    let loginDisplay = document.querySelector('#loginDisplay');
+    loginDisplay.classList.add('hide');
+
+    let signUpPage = document.querySelector('#signUpPage');
+    signUpPage.classList.remove('hide')
+}
+
+
+
+//! proof of concept =================================
+
 let button = document.querySelector('#names')
 
 button.addEventListener('click', () => {
@@ -39,36 +97,4 @@ function displayEmails(data) {
         div.textContent = data[i].email;
         resultContainer.appendChild(div)
     }
-}
-
-
-let loginButton = document.querySelector('#loginButton')
-loginButton.addEventListener('click', login)
-
-function login() {
-    let inputUserName = document.querySelector('#username')
-    console.log(inputUserName.value);
-    let inputPassword = document.querySelector('#password')
-    console.log(inputPassword.value);
-
-    fetch('http://localhost:8000/api/users')
-        .then(response => response.json())
-        .then(data => authenticate(data))
-
-    for (let i = 0; i < data.length; i++) {
-        const element = data[i];
-
-    }
-
-
-
-    // let homePage = document.querySelector('#homePage')
-    // homePage.classList.remove('hide')
-
-    // let loginPage = document.querySelector('#loginPage')
-    // loginPage.classList.add('hide')
-}
-
-function authenticate(data) {
-
 }
