eb69d3ac764de46c1220505edc19360df6087127
Neo Temores <neo@Mac.lan>
2022-05-24 09:10:19 -0500

    working controller

--- controller.js
+++ controller.js
@@ -11,6 +11,23 @@ const getAllUsers = async (req, res) => {
     }
 }
 
+const createNewUser = async (req, res) => {
+    let firstName = req.body.firstName
+    let lastName = req.body.lastName
+    let userName = req.body.userName
+    let email = req.body.email
+    let password = req.body.password
+
+    try {
+        await pool.connect();
+        await pool.query('INSERT INTO users (first_name, last_name, user_name, email, password) VALUES($1, $2, $3, $4, $5);', [firstName, lastName, userName, email, password])
+        res.json(req.body)
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
 const getAllPosts = async (req, res) => {
     try {
         await pool.connect();
@@ -24,5 +41,6 @@ const getAllPosts = async (req, res) => {
 
 module.exports = {
     getAllUsers,
-    getAllPosts
+    createNewUser,
+    getAllPosts,
 }
--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 require('dotenv').config();
 const express = require('express');
-const { application_name } = require('pg/lib/defaults');
 const app = express();
-const pool = require('./connection');
-// const controller = require('./controller.js')
+// const pool = require('./connection');
+const controller = require('./controller.js')
 
 const PORT = process.env.PORT;
 
@@ -14,22 +13,21 @@ app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 })
 
-// app.get('/api/users', controller.getAllUsers)
+
+app.get('/api/users', controller.getAllUsers);
+
+app.post('/api/users/create', controller.createNewUser)
+
+
+
+
+
 
 // app.get('/api/posts', controller.getAllPosts)
 
 
 
-app.get('/api/users', async (req, res) => {
-    try {
-        await pool.connect();
-        let data = await pool.query('SELECT * FROM users;')
-        res.json(data.rows)
-
-    } catch (error) {
-        console.log(error)
-    }
-})
+// app.get('/api/users', )
 
 app.get('/api/posts', async (req, res) => {
     try {
@@ -42,18 +40,5 @@ app.get('/api/posts', async (req, res) => {
     }
 })
 
-app.post('/api/users/create', async (req, res) => {
-    let firstName = req.body.firstName
-    let lastName = req.body.lastName
-    let userName = req.body.userName
-    let email = req.body.email
-    let password = req.body.password
-
-    try {
-        await pool.connect();
-        await pool.query('INSERT INTO users (first_name, last_name, user_name, email, password) VALUES($1, $2, $3, $4, $5);', [firstName, lastName, userName, email, password])
-        res.json(req.body)
-    } catch (error) {
-        console.error(error);
-    }
-}) 
+// app.post('/api/users/create', async (req, res) => {
+//    ) 
