09956e9b9418540dff36c44269555449e855cdeb
Neo Temores <neo@Mac.lan>
2022-05-25 23:12:13 -0500

    deploy

--- db/seed.sql
+++ db/seed.sql
@@ -8,12 +8,12 @@ VALUES
 
 INSERT INTO posts (post_content, datetime, user_id)
 VALUES
-('Hi my name is neo!', '5/23/2022, 4:54:33 PM', 1),
-('Hello I am kait!','5/23/2022, 5:24:53 PM', 2),
-('I am baby leo','5/23/2022, 7:25:26 PM', 3),
-('Im a dog! duh...','5/24/2022, 1:24:55 PM',  4),
-('I like dogs','5/24/2022, 8:14:32 PM', 1),
-('I like cats more','5/24/2022, 9:44:23 PM', 2),
-('I like to drink milk','5/25/2022, 2:44:33 PM', 3),
-('bark, bark!','5/25/2022, 3:36:13 PM', 4);
+('Elit ut aliquam purus sit amet luctus venenatis. Condimentum id venenatis a condimentum vitae sapien. Eget arcu dictum varius duis at consectetur. ', '5/23/2022, 4:54:33 PM', 1),
+('Semper risus in hendrerit gravida rutrum quisque. Volutpat diam ut venenatis tellus. ','5/23/2022, 5:24:53 PM', 2),
+('Viverra accumsan in nisl nisi scelerisque eu ultrices. Egestas maecenas pharetra convallis posuere morbi. Egestas sed tempus urna et pharetra pharetra massa massa ultricies. Sit amet porttitor eget dolor morbi non arcu risus.','5/23/2022, 7:25:26 PM', 3),
+('Amet justo donec enim diam vulputate ut. Sed vulputate odio ut enim blandit volutpat maecenas volutpat blandit.','5/24/2022, 1:24:55 PM',  4),
+('Id porta nibh venenatis cras. Et sollicitudin ac orci phasellus. Mi sit amet mauris commodo quis.','5/24/2022, 8:14:32 PM', 1),
+('Felis eget nunc lobortis mattis aliquam faucibus purus in massa. Commodo nulla facilisi nullam vehicula ipsum a arcu cursus vitae. Vel quam elementum pulvinar etiam non quam.','5/24/2022, 9:44:23 PM', 2),
+(' Sem nulla pharetra diam sit amet nisl suscipit. Eu scelerisque felis imperdiet proin. Eu nisl nunc mi ipsum faucibus vitae aliquet. Vitae ultricies leo integer malesuada.','5/25/2022, 2:44:33 PM', 3),
+('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae nunc sed velit dignissim. Proin nibh nisl condimentum id venenatis a condimentum.','5/25/2022, 3:36:13 PM', 4);
 
